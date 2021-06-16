8ff0214692a5dafcff553b3f47475b9eceb21ddd
DB ROBOTS <85598210+DB-ROBOTS@users.noreply.github.com>
2021-06-16 12:23:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def main():
     with user_client:
         while True:
             print("[INFO] starting to check uptime..")
-            edit_text = f"🔰𝐓𝐫𝐚𝐜𝐤𝐬𝐭𝐮𝐝𝐢𝐨'𝐬 𝐁𝐨𝐭𝐬 𝐒𝐭𝐚𝐭𝐮𝐬🔰\n\n__( All bots are checked automatically if any correction report it )__\n\n\n"
+            edit_text = f"🔰𝐃𝐁 𝐑𝐎𝐁𝐎𝐓𝐒 𝐁𝐎𝐓'𝐒 𝐒𝐓𝐀𝐓𝐔𝐒🔰\n\n__( All bots are checked automatically if any correction report it )__\n\n\n"
             for bot in bots:
                 print(f"[INFO] checking @{bot}")
                 snt = user_client.send_message(bot, '/start')
@@ -31,18 +31,18 @@ def main():
                 msg = user_client.get_history(bot, 1)[0]
                 if snt.message_id == msg.message_id:
                     print(f"[WARNING] @{bot} is down")
-                    edit_text += f"𝙱𝙾𝚃 𝙽𝙰𝙼𝙴    {bot} \n𝚄𝚂𝙴𝚁𝙽𝙰𝙼𝙴  @{bot}\n𝚂𝚃𝙰𝚃𝚄𝚂 ❌\n\n"
+                    edit_text += f"<b>♨ ʙᴏᴛ ɴᴀᴍᴇ ☞{bot} \n♛ ᴜsᴇʀɴᴀᴍᴇ ☞ @{bot}\n♽ ʙᴏᴛ's sᴛᴀᴜs ☞ ☒ ☠ ☒\n\n</b>"
                     #user_client.send_message("me",
                                              #f"@{bot} was down")
                 else:
                     print(f"[INFO] all good with @{bot}")
-                    edit_text += f"𝙱𝙾𝚃 𝙽𝙰𝙼𝙴    {bot} \n𝚄𝚂𝙴𝚁𝙽𝙰𝙼𝙴  @{bot}\n𝚂𝚃𝙰𝚃𝚄𝚂 ✅\n\n"
+                    edit_text += f"<b>♨ ʙᴏᴛ ɴᴀᴍᴇ ☞{bot}\n♛ ᴜsᴇʀɴᴀᴍᴇ ☞ @{bot}\n♽ ʙᴏᴛ's sᴛᴀᴛᴜs ☞ ☑ ☻ ☑\n\n</b>"
                 user_client.read_history(bot)
 
             time_now = datetime.datetime.now(pytz.timezone('Asia/Kolkata'))
             formatted_time = time_now.strftime("%d %B %Y %I:%M %p")
 
-            edit_text += f"**Updated on {formatted_time} (**IST**)**"
+            edit_text += f"**𝙻𝙰𝚂𝚃 𝚄𝙿𝙳𝙰𝚃𝙴𝙳 𝙾𝙽 {formatted_time} (**IST**)**"
 
             for status_message_id in status_message_ids:
                 user_client.edit_message_text(int(update_channel), status_message_id,
