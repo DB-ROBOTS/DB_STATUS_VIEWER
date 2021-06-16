af82eb31ef3cee9aa6661c61429aab9fb0e40514
DB ROBOTS <85598210+DB-ROBOTS@users.noreply.github.com>
2021-06-16 13:08:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from pyrogram import Client
 user_session_string = os.environ.get("user_session_string")
 bots = [i.strip() for i in os.environ.get("bots").split(' ')]
 update_channel = os.environ.get("update_channel")
-status_message_ids = [int(i.strip()) for i in os.environ.get("status_message_id").split('9')]
+status_message_ids = [int(i.strip()) for i in os.environ.get("9").split('')]
 api_id = int(os.environ.get("api_id"))
 api_hash = os.environ.get("api_hash")
 user_client = Client(session_name=str(user_session_string), api_id=api_id, api_hash=api_hash)
