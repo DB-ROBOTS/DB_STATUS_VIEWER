4f863cd697a3c88a78f26f12526a5d630c916d1e
DB ROBOTS <85598210+DB-ROBOTS@users.noreply.github.com>
2021-06-16 18:06:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,18 +31,18 @@ def main():
                 msg = user_client.get_history(bot, 1)[0]
                 if snt.message_id == msg.message_id:
                     print(f"[WARNING] @{bot} is down")
-                    edit_text += f"<b>♨ ʙᴏᴛ ɴᴀᴍᴇ ☞{bot} \n♛ ᴜsᴇʀɴᴀᴍᴇ ☞ @{bot}\n♽ ʙᴏᴛ's sᴛᴀᴜs ☞ ☒ ☠ ☒\n\n</b>"
+                    edit_text += f"<b>♨ ʙᴏᴛ ɴᴀᴍᴇ ☞{bot} \n♛ ᴜsᴇʀɴᴀᴍᴇ ☞ @{bot}\n♽ ʙᴏᴛ's sᴛᴀᴜs ☞ ❎ ☠ ❎\n\n</b>"
                     #user_client.send_message("me",
                                              #f"@{bot} was down")
                 else:
                     print(f"[INFO] all good with @{bot}")
-                    edit_text += f"<b>♨ ʙᴏᴛ ɴᴀᴍᴇ ☞{bot}\n♛ ᴜsᴇʀɴᴀᴍᴇ ☞ @{bot}\n♽ ʙᴏᴛ's sᴛᴀᴛᴜs ☞ ☑ ☻ ☑\n\n</b>"
+                    edit_text += f"<b>♨ ʙᴏᴛ ɴᴀᴍᴇ ☞{bot}\n♛ ᴜsᴇʀɴᴀᴍᴇ ☞ @{bot}\n♽ ʙᴏᴛ's sᴛᴀᴛᴜs ☞ ☑️ ☻ ☑️\n\n</b>"
                 user_client.read_history(bot)
 
             time_now = datetime.datetime.now(pytz.timezone('Asia/Kolkata'))
             formatted_time = time_now.strftime("%d %B %Y %I:%M %p")
 
-            edit_text += f".\n\n**𝙻𝙰𝚂𝚃 𝚄𝙿𝙳𝙰𝚃𝙴𝙳 𝙾𝙽 {formatted_time} (**IST**)**.\n"
+            edit_text += f"**𝙻𝙰𝚂𝚃 𝚄𝙿𝙳𝙰𝚃𝙴𝙳 𝙾𝙽 {formatted_time} (**IST**)**.\n"
 
             for status_message_id in status_message_ids:
                 user_client.edit_message_text(int(update_channel), status_message_id,
